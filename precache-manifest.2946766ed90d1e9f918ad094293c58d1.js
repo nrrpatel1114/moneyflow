a4d44f63891be3729644264e106d175f8ad0b04a
nrrpatel <nickpatelbhadol@gmail.com>
2022-06-24 13:05:19 -0400

    Updates

---
+++ precache-manifest.2946766ed90d1e9f918ad094293c58d1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8de32e9bd150210dfdfd",
+    "url": "/moneyflow/static/css/main.2d0c49d3.chunk.css"
+  },
+  {
+    "revision": "8de32e9bd150210dfdfd",
+    "url": "/moneyflow/static/js/main.8de32e9b.chunk.js"
+  },
+  {
+    "revision": "6c00ac662e5a322cc349",
+    "url": "/moneyflow/static/js/1.6c00ac66.chunk.js"
+  },
+  {
+    "revision": "8246a422b70153aaf0bc",
+    "url": "/moneyflow/static/css/2.53299cfa.chunk.css"
+  },
+  {
+    "revision": "8246a422b70153aaf0bc",
+    "url": "/moneyflow/static/js/2.8246a422.chunk.js"
+  },
+  {
+    "revision": "d0120d225b962d375e8f",
+    "url": "/moneyflow/static/js/runtime~main.d0120d22.js"
+  },
+  {
+    "revision": "8e84ba904221b232947254289191825e",
+    "url": "/moneyflow/static/media/healthcare.8e84ba90.png"
+  },
+  {
+    "revision": "eb3f7b3f4e48354a4f003a43e3f7166c",
+    "url": "/moneyflow/static/media/shopping.eb3f7b3f.png"
+  },
+  {
+    "revision": "9c322a01d078513f64c16efe7bee9df6",
+    "url": "/moneyflow/static/media/food.9c322a01.png"
+  },
+  {
+    "revision": "75cfceaaf8891d080f99ec0d64305f26",
+    "url": "/moneyflow/index.html"
+  }
+];
