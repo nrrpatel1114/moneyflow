17b3fde6989d0d2e5e7436dcd9dc3595304f9a00
nrrpatel <nickpatelbhadol@gmail.com>
2022-06-24 21:34:49 -0400

    Updates

---
+++ precache-manifest.df9a6ebfece32c666acba5e21796b107.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "aadf5703b96003b5be7c",
+    "url": "/username.github.io/static/css/main.204345b6.chunk.css"
+  },
+  {
+    "revision": "aadf5703b96003b5be7c",
+    "url": "/username.github.io/static/js/main.aadf5703.chunk.js"
+  },
+  {
+    "revision": "6c00ac662e5a322cc349",
+    "url": "/username.github.io/static/js/1.6c00ac66.chunk.js"
+  },
+  {
+    "revision": "8246a422b70153aaf0bc",
+    "url": "/username.github.io/static/css/2.53299cfa.chunk.css"
+  },
+  {
+    "revision": "8246a422b70153aaf0bc",
+    "url": "/username.github.io/static/js/2.8246a422.chunk.js"
+  },
+  {
+    "revision": "67a7c41c1b55faa29825",
+    "url": "/username.github.io/static/js/runtime~main.67a7c41c.js"
+  },
+  {
+    "revision": "8e84ba904221b232947254289191825e",
+    "url": "/username.github.io/static/media/healthcare.8e84ba90.png"
+  },
+  {
+    "revision": "eb3f7b3f4e48354a4f003a43e3f7166c",
+    "url": "/username.github.io/static/media/shopping.eb3f7b3f.png"
+  },
+  {
+    "revision": "9c322a01d078513f64c16efe7bee9df6",
+    "url": "/username.github.io/static/media/food.9c322a01.png"
+  },
+  {
+    "revision": "ff618bb35b49f6c1f8e521c3d7cee612",
+    "url": "/username.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/moneyflow/precache-manifest.f41a15498491ba95ddfd7ec0fcebcb7f.js"
+  "/username.github.io/precache-manifest.df9a6ebfece32c666acba5e21796b107.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/moneyflow/index.html", {
+workbox.routing.registerNavigationRoute("/username.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
