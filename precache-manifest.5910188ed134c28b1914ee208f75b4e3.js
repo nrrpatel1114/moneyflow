3786d5f8cef602a981bcd1a0fc662e44d406b137
nrrpatel <nickpatelbhadol@gmail.com>
2022-06-24 21:52:53 -0400

    Updates

---
+++ precache-manifest.5910188ed134c28b1914ee208f75b4e3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9cbfa94aac3da4059e1",
+    "url": "/moneyflow/static/css/main.204345b6.chunk.css"
+  },
+  {
+    "revision": "a9cbfa94aac3da4059e1",
+    "url": "/moneyflow/static/js/main.a9cbfa94.chunk.js"
+  },
+  {
+    "revision": "6c00ac662e5a322cc349",
+    "url": "/moneyflow/static/js/1.6c00ac66.chunk.js"
+  },
+  {
+    "revision": "8246a422b70153aaf0bc",
+    "url": "/moneyflow/static/css/2.53299cfa.chunk.css"
+  },
+  {
+    "revision": "8246a422b70153aaf0bc",
+    "url": "/moneyflow/static/js/2.8246a422.chunk.js"
+  },
+  {
+    "revision": "d0120d225b962d375e8f",
+    "url": "/moneyflow/static/js/runtime~main.d0120d22.js"
+  },
+  {
+    "revision": "8e84ba904221b232947254289191825e",
+    "url": "/moneyflow/static/media/healthcare.8e84ba90.png"
+  },
+  {
+    "revision": "eb3f7b3f4e48354a4f003a43e3f7166c",
+    "url": "/moneyflow/static/media/shopping.eb3f7b3f.png"
+  },
+  {
+    "revision": "9c322a01d078513f64c16efe7bee9df6",
+    "url": "/moneyflow/static/media/food.9c322a01.png"
+  },
+  {
+    "revision": "28c3869593451f634ce975028d0cf4f6",
+    "url": "/moneyflow/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nrrpatel1114.github.io/precache-manifest.132c9b012f44fab1bef35b367f51e8a5.js"
+  "/moneyflow/precache-manifest.5910188ed134c28b1914ee208f75b4e3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nrrpatel1114.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/moneyflow/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
