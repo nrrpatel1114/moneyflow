ef0202e497385e3b8221bfcdb2981083717b7ac4
nrrpatel <nickpatelbhadol@gmail.com>
2022-06-24 22:06:29 -0400

    oiusfgr

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./pages/home/index";
 
 const App = () => {
   return (
-    <Router basename="/moneyflow/add-expense" >
+    <Router basename="/" >
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
