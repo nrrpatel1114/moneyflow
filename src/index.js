ec37db83e5fff755c8115b4168e1ffe37d3dcd78
nrrpatel <nickpatelbhadol@gmail.com>
2022-06-24 22:40:19 -0400

    erwt

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/index";
 
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
