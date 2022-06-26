ea3c47aba56fabdc9a50fc4d2c78b9c66e7abc68
nrrpatel <nickpatelbhadol@gmail.com>
2022-06-25 23:39:02 -0400

    browserrouter update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import AddExpense from "./pages/add-expense";
 import Home from "./pages/home/index";
-import { HashRouter } from "react-router-dom";
+
 
 const App = () => {
   return (
