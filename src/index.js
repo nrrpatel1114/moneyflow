d2e689878f6e04fa163c5cae1fd3f9c1af13c3ed
nrrpatel <nickpatelbhadol@gmail.com>
2022-06-27 22:03:13 -0400

    updated for iphone 12 test #1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
-import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store/index";
 
